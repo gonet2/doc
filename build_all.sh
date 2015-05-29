52ac26a872e0437c60ba496d4f05b489216fb301
xtaci <daniel820313@gmail.com>
2015-05-29 04:28:36 +0000

    add sh

---
+++ build_all.sh
@@ -0,0 +1,28 @@
+cd ..
+cd ./bgsave
+source gvp
+gpm
+go install bgsave
+sudo docker build -t xtaci/bgsave .
+cd ..
+
+cd ./snowflake
+source gvp
+gpm
+go install snowflake
+sudo docker build -t xtaci/snowflake .
+cd ..
+
+cd ./geoip
+source gvp
+gpm
+go install geoip
+sudo docker build -t xtaci/geoip .
+cd ..
+
+cd ./wordfilter
+source gvp
+gpm
+go install wordfilter
+sudo docker build -t xtaci/wordfilter .
+cd ..
---
+++ pull_all.sh
@@ -0,0 +1,16 @@
+cd ..
+cd ./bgsave
+git pull
+cd ..
+
+cd ./snowflake
+git pull
+cd ..
+
+cd ./geoip
+git pull
+cd ..
+
+cd ./wordfilter
+git pull
+cd ..
