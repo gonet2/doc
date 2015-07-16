c64b3f79d656f3a1ef78b6f1a646161c4f0ede2d
luo Jun <johnnyluo586@gmail.com>
2015-07-16 14:35:04 +0800

    remove not necessary

--- base_service.sh
+++ base_service.sh
@@ -1,31 +1,22 @@
 #!/bin/bash -e
 
 #start all base service.
-#default is localhost:4001,localhost:2379, it's can't forward port to host ip.
-#all the program need to copy in to GOBIN
-export GOBIN=/go/bin
+#etcd default is localhost:4001,localhost:2379, it's can't forward port to host ip.
 export ETCD_ADDR="172.17.42.1:2379"
 case $1 in
 	start)
-		#1. mongodb listen : 27017
-		#sudo service mongod start
-		#2. redis listen : 6379
-		#/usr/local/bin/redis-server /go/redis/conf/redis.conf &
-		#3. nsq
-		#nsqlookup tcp: 0.0.0.0:4160, http: 0.0.0.0:4161
-		$GOBIN/nsqlookupd --tcp-address=172.17.42.1:4160 --http-address=172.17.42.1:4161 &
-		#nsqd tcp: 0.0.0.0:4150, http: 0.0.0.0:4151
-		$GOBIN/nsqd --lookupd-tcp-address=172.17.42.1:4160 --tcp-address=172.17.42.1:4150 --http-address=172.17.42.1:4151 &
-		#nsqadmin http: 0.0.0.0:4171
-		$GOBIN/nsqadmin --lookupd-http-address=172.17.42.1:4161 --http-address=172.17.42.1:4171 &
-		#4. etcd 4001 for client 
-		$GOBIN/etcd &
-		#5. gliderlabs/registrator
+		
+		nsqlookupd --tcp-address=172.17.42.1:4160 --http-address=172.17.42.1:4161 &
+		
+		nsqd --lookupd-tcp-address=172.17.42.1:4160 --tcp-address=172.17.42.1:4150 --http-address=172.17.42.1:4151 &
+		
+		nsqadmin --lookupd-http-address=172.17.42.1:4161 --http-address=172.17.42.1:4171 &
+		
+		etcd &
+		
 		docker run -d -v /var/run/docker.sock:/tmp/docker.sock gliderlabs/registrator -ip="public_ip_that_all_services_can_access" etcd://172.17.42.1:2379/backends
 		;;
 	stop)
-		#sudo service mongod stop
-		#killall redis-server
 		killall nsqlookupd
 		killall nsqd
 		killall nsqadmin
